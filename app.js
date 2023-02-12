90834fb18507a8bb9b872d6f10dec5356a659c22
dusdn3441 <121365615+dusdn3441@users.noreply.github.com>
2023-02-11 16:07:41 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+const modeBtn = document.getElementById("mode-btn");
+
 const colorOption = Array.from(
   document.getElementsByClassName("color-option")
 );
-
 
 const color = document.getElementById("color");
  const lineWidth = document.getElementById("line-width");
@@ -11,6 +12,8 @@ canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
+
 function onMove(event) {
   if (isPainting) {
     ctx.lineTo(event.offsetX, event.offsetY);
@@ -43,7 +46,24 @@ function cancelPainting() {
   color.value = colorValue;
   
  }
+ function onModeClick(){
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+  }
  
+ 
+  function onCanvasClick(){
+  if(isFilling) {
+    ctx.fillRect(0, 0, 800, 800);
+  }
+  }
+
+ canvas.addEventListener("click", onCanvasClick);
  
  canvas.addEventListener("mousemove", onMove);
  canvas.addEventListener("mousedown", startPainting);
@@ -54,3 +74,5 @@ function cancelPainting() {
  color.addEventListener("change", onColorChange);
 
  colorOption.forEach((color) => color.addEventListener("click", onColorClick));
+ modeBtn.addEventListener("click", onModeClick);
+
